95ff752a32e08fa7397f6a78efb3fc655a29a86c
visualstudio <visualstudio@122-PC05>
2019-03-08 18:34:04 -0500

    Lab1: Working with Delegates Working

---
+++ C0732104-Assignment-2-CSD3354-2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0732104_Assignment_2_CSD3354_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
