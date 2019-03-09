301a52fe2289629eb622505d05e320b3e3137f36
visualstudio <visualstudio@122-PC05>
2019-03-08 20:15:24 -0500

    Lab 11 Use the debugger to report the results of running this code

--- C0732104-Assignment-2-CSD3354-2/Program.cs
+++ C0732104-Assignment-2-CSD3354-2/Program.cs
@@ -11,32 +11,13 @@ namespace C0732104_Assignment_2_CSD3354_2
 //March 8,2019
 //Csd3354-2
 {
-    public class Program
-    {
-        public static void Main()
-        {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-    }
-
-    public delegate void MyDelegate();
-
     public class DelegateExercises
     {
-        void Method1()
+        public object MyDelegate { get; private set; }
+
+        void Method3()
         {
-            System.Console.WriteLine("Method1");
-
-
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-            System.Console.WriteLine(myDelegate.ToString());
+            Console.WriteLine(MyDelegate.ToString());
         }
     }
 }
